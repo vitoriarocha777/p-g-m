1941d34c56e0b954da667f28aea15a5e0ca9567c
vitoriarocha777 <vitoria.rocha29@escola.pr.gov.br>
2026-03-05 09:27:51 -0300

    Add files via upload

---
+++ dodo/aula1.py
@@ -0,0 +1,20 @@
+total_de_tentativas = 3
+
+while (total_de_tentativas > 0):
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
+
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+print("Fim do jogo")
